55f6783000066bf4a80a0b9e4fd2fa5b0653eb6b
prashant <pkcsmath@gmail.com>
2023-01-22 00:00:02 +0530

    commit changes

---
+++ src/main/java/selenium/LaunchAddressbook.java
@@ -0,0 +1,34 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchAddressbook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.setProperty("webdriver.chrome.driver", "C:\\Users\\pkcsm\\Downloads\\chromedriver_win32\\chromedriver.exe");
+       WebDriver  driver = new ChromeDriver();
+       driver.get("http://184.73.138.187:8080/addressbook/");
+       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		driver.findElement(By.className("v-button")).click();
+		driver.findElement(By.id("gwt-uid-5")).sendKeys("suragimath");
+		driver.findElement(By.id("gwt-uid-7")).sendKeys("vihaan");
+		driver.findElement(By.id("gwt-uid-9")).sendKeys("9886087613");
+		driver.findElement(By.id("gwt-uid-11")).sendKeys("pkcsmath@gmail.com");
+		driver.findElement(By.id("gwt-uid-13")).sendKeys("28/02/2022");
+		
+		//click on the save button
+		driver.findElement(By.className("v-button-primary")).click();
+		
+		
+		driver.quit();
+		System.out.println("Script executed successfully");
+	}
+
+}
